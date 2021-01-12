b6f3a6a652b0d87cd43e217e16032a4f20e5c509
Clems-web <clement.servais1@gmail.com>
2021-01-12 15:48:18 +0100

    Finito !

--- app.js
+++ app.js
@@ -35,5 +35,35 @@ $("body").keydown(function(event) {
         $("audio").get(8).play();
         $("div[data-key=76]").addClass("playing");
     }
+}).keyup(function(event) {
+    if (event.which === 65) {
+        $("div[data-key=65]").removeClass("playing");
+    }
+    if (event.which === 83) {
+        $("div[data-key=83]").removeClass("playing");
+    }
+    if (event.which === 68) {
+        $("div[data-key=68]").removeClass("playing");
+    }
+    if (event.which === 70) {
+        $("div[data-key=70]").removeClass("playing");
+    }
+    if (event.which === 71) {
+        $("div[data-key=71]").removeClass("playing");
+    }
+    if (event.which === 72) {
+        $("div[data-key=72]").removeClass("playing");
+    }
+    if (event.which === 74) {
+        $("div[data-key=74]").removeClass("playing");
+    }
+    if (event.which === 75) {
+        $("div[data-key=75]").removeClass("playing");
+    }
+    if (event.which === 76) {
+        $("div[data-key=76]").removeClass("playing");
+    }
 })
+
+
 
